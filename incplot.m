5c95dca01ad1d85b13ad3b487decaa0e38ea3952
jerivington <james.rivington1@gmail.com>
2017-07-07 10:43:20 +0100

    Update incplot.m

--- incplot.m
+++ incplot.m
@@ -1,54 +1,61 @@
 function incplot(min,max,parameter,s,k,r,t,st,n)
-
+ 
 % Make sure function logcallprice is saved in matlab directory before using
-
+ 
+% This function increments a parameter between a minimum and maximum value
+% for 20 increments, evaluates the monte carlo price for the log option and
+% plots the price against the parameter.
+ 
 % min is minimum incremented value
 % max is maximum incremented value
-
+ 
 % parameter selects icremented parameter to increment
 % underlying - parameter = 1
 % strike - parameter = 2 
 % rate - parameter = 3 
 % time - parameter = 4 
 % stdv - parameter = 5
-
-% s,k,r,t,st,n are base paramters for Monte Carlo when not being
-% incremented. s - underlying, k - strike, r - rate, t - time to maturity,
+ 
+% s,k,r,t,st,n are base paramters for Monte Carlo when not being incremented.
+% s - underlying, k - strike, r - rate, t - time to maturity,
 % st - standard deviation, n - simulations.
-
+ 
+ 
 index = 1; % index for storage vector
-
+ 
 % predeclaring storage vector
-
+ 
 storage = zeros(1,length(min:((max-min)/15):max));
-
+ 
 if parameter == 1;
     
-    %underlying variation
+    % underlying variation
     
     for i = min:((max-min)/20):max
         
-    %incrementing and storing
+    % incrementing, evaluating monte carlo and storing
     
     storage(1,index)=logcallprice(i,k,r,t,st,n);
     index=index+1;
-
+ 
     end
     
-    %plotting increments
+    % Plotting increments vs monte carlo prices
     
     plot(min:((max-min)/20):max,storage(1,:));
     xlabel('Initial Underlying Value')
-
+ 
     % This procedure is the same for every other parameter selection below
     
 elseif parameter == 2;
     
+    % Strike price variation
+    
     for i = min:((max-min)/20):max
-
+ 
     storage(1,index)=logcallprice(s,i,r,t,st,n);
     index=index+1;
-
+ 
     end
     
     plot(min:((max-min)/20):max,storage(1,:));
@@ -56,23 +63,27 @@ elseif parameter == 2;
     
 elseif parameter == 3;
     
+    % rate variation
+    
     for i = min:((max-min)/20):max
     
     storage(1,index)=logcallprice(s,k,i,t,st,n);
     index=index+1;
-
+ 
     end
     
     plot(min:((max-min)/20):max,storage(1,:));
     xlabel('Rate')
-
+ 
 elseif parameter == 4;
+    
+    % maturity time variation
     
     for i = min:((max-min)/20):max
     
     storage(1,index)=logcallprice(s,k,r,i,st,n);
     index=index+1;
-
+ 
     end
     
     plot(min:((max-min)/20):max,storage(1,:));
@@ -80,11 +91,13 @@ elseif parameter == 4;
     
 elseif parameter == 5;
     
+    % standard deviation variation
+    
     for i = min:((max-min)/20):max
     
     storage(1,index)=logcallprice(s,k,r,t,i,n);
     index=index+1;
-
+ 
     end
     
     plot(min:((max-min)/20):max,storage(1,:));
@@ -97,5 +110,5 @@ else
     display('parameter input not valid')   
     
 end
-
+ 
 end
