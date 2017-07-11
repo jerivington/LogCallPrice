1f2f94906e35dd6484ad95a920c7b50e0d565073
jerivington <james.rivington1@gmail.com>
2017-07-11 10:19:31 +0100

    Delete analytical_price.m

--- analytical_price.m
+++
@@ -1,3 +0,0 @@
-% Analytical Price of option with payoff=max(log(s)-log(e),0)
-
-V=@(s,e,r,t,st)(exp(-r.*t).*( (log(s./e)+(r-0.5.*st.^2).*t).*normcdf(((log(s./e)+(r-0.5.*st.^2).*t)./(st.*(t.^0.5)))) +(st./((2.*pi).^0.5)).*(t.^0.5).*exp(-(((log(s./e)+(r-0.5.*st.^2).*t)./(st.*(t.^0.5))).^2)./2)));
